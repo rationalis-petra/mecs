94c583362fcb5a8fd4e9409c6c0382d3019c6857
rationalis-petra <connor.redfern@optusnet.com.au>
2020-06-19 21:00:16 +0800

    Ditto

---
+++ include/components/camera.h
@@ -0,0 +1,28 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+/** @file
+ * @brief contains the camera struct
+ */
+
+///@addtogroup Components
+///@{
+
+/** @brief The camera struct allows a camera to be attached to the entity which
+ *         contains it.
+ *
+ * @details The camera struct contains information about it's position relative
+ *          to a target
+ */
+typedef struct Camera {
+  float theta;   ///< The angle of inclination from the camera to the target
+  float phi;     ///< The azmithal angle from the x-axis to the target
+  float r;       ///< The spherical radial distance from the player to the camera
+} Camera;
+
+void* new_camera();
+
+void delete_camera(void* _camera);
+///@}
+
+#endif
