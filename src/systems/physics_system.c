342a187b5d4a7b573e5988c63094aab57438b07a
rationalis-petra <connor.redfern@optusnet.com.au>
2020-06-27 10:38:44 +0800

    Added collision detection and (buggy) compensation

--- src/systems/physics_system.c
+++ src/systems/physics_system.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <math.h>
 
 #include "engine.h"
 #include "components/components.h"
@@ -28,7 +29,7 @@ void physics_system() {
 
         // gravity:
         if (transform->position.y > 0.1f) {
-           rigidbody->velocity.y -= 9.8 * args.dt;
+            rigidbody->velocity.y -= 9.8 * args.dt;
         }
         else {
             rigidbody->velocity.y = rigidbody->velocity.y > 0 ? rigidbody->velocity.y : 0;
@@ -37,6 +38,41 @@ void physics_system() {
         transform->position = vec3f_sum(transform->position, vec3f_multiply(args.dt, rigidbody->velocity));
         if (transform->position.y < 0) { transform->position.y = 0; }
 
+
+        // for now, we use two-stage collision system:
+        // stage one: compare collision spheres - fastest & least granular option
+        // stage two: compare collision cubes - slower & less granular
+        Vec3f m_centre = transform->position;
+        for (int id2 = 0; id2 < physicals.len; id2++) {
+            if (id == id2) continue;
+            Transform* transform2 = get_component(id2, TransformType);
+            Vec3f o_centre = transform2->position;
+
+            // we test each of the following for bounding box overlap
+            // we consider them to be unit cubes
+            Vec3f diff_vec = vec3f_difference(o_centre, m_centre);
+            if (fabs(diff_vec.x) < 1.0f &&
+                fabs(diff_vec.y) < 1.0f &&
+                fabs(diff_vec.z) < 1.0f) {
+                // we want to solve the equation for the intersectino of a line with
+                // an axis-aligned plane - plane depends on sign of diff_vec
+                float px = diff_vec.x < 0.0f ? 0.5f : -0.5f;
+                float py = diff_vec.y < 0.0f ? 0.5f : -0.5f;
+                float pz = diff_vec.z < 0.0f ? 0.5f : -0.5f;
+
+                float lx = (o_centre.x + px - m_centre.x) / diff_vec.x;
+                float ly = (o_centre.y + py - m_centre.y) / diff_vec.y;
+                float lz = (o_centre.z + pz - m_centre.z) / diff_vec.z;
+
+                float delta;
+                if (lx > 0.0f && lx < 0.5f) {delta = 0.5f - lx;}
+                else if (ly > 0.0f && ly < 0.5f) {delta = 0.5f - ly;}
+                else if (lz > 0.0f && lz < 0.5f) {delta = 0.5f - lz;}
+
+                transform2->position = vec3f_sum(o_centre, vec3f_multiply(delta, diff_vec));
+                transform->position = vec3f_sum(m_centre, vec3f_multiply(-delta, diff_vec));
+            }
+        }
     }
 }
 
